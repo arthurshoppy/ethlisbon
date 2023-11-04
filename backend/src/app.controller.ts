236ae659b03cfd25c884a89430f8c9bb4fbbf92c
Fabbaist <weberfabian1@gmx.de>
2023-11-04 19:05:17 +0100

    Set up mongoDB with survey and response schema, cleaned authentication up

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Survey } from './dtos/app.survey';
+
 
 @Controller()
 export class AppController {
@@ -9,14 +9,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-  @Get()
-  getSurveyResults(): string {
-    return this.appService.getSurveyResults();
-  }
-
-  @Post()
-  postNewSurvey(): Survey {
-    return this.appService.postNewSurvey();
   }
 }
--- backend/src/survey/survey.controller.ts
+++ backend/src/survey/survey.controller.ts
@@ -2,15 +2,18 @@
 import { Controller, Post, Body, Get, Param, UseGuards } from '@nestjs/common';
 import { SurveyService } from './survey.service';
 import { AuthGuard } from '@nestjs/passport';
+import { CreateSurveyDto } from './dtos/create-survey.dto';
+import { SurveyResponseDto } from './dtos/survey-response.dto';
 
 @Controller('surveys')
 export class SurveysController {
   constructor(private readonly surveyService: SurveyService) {}
 
-  // UseGuards with an AdminGuard to protect this route for admin use only
+
   @Post()
-  @UseGuards(AuthGuard('jwt'), AdminGuard)
+  //auth token
   async createSurvey(@Body() body: any) {
+    // UUID for survey
     return this.surveyService.createSurvey(body.title, body.questions);
   }
 
@@ -18,10 +21,16 @@ export class SurveysController {
   async getSurvey(@Param('id') id: string) {
     return this.surveyService.getSurvey(parseInt(id));
   }
+  @Get (':id/my-survey')
+  async getMySurvey(){
+    // UUID of survey matching with user address. 
+    return ;
+  }
 
   // Save the user's response to a survey
   @Post(':id/response')
-  async saveSurveyResponse(@Param('id') id: string, @Body() body: any) {
+  //auth token
+  async saveSurveyResponse(@Param('id') id: string, @Body() body: SurveyResponseDto) {
     return this.surveyService.saveResponse(parseInt(id), body.responses);
   }
 }
