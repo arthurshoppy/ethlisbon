f1950384309c16660085166811e37b4d0bb1d312
Fabbaist <weberfabian1@gmx.de>
2023-11-04 11:31:29 +0100

    Added nestjs backend

---
+++ backend/src/app.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  authUser(): string {
+    // implement usersignup with credentials
+    const userAddress = '0x1';
+    return userAddress;
+  }
+
+  surveyResults(): string {
+
+    return const result: string;
+  }
+
+}
